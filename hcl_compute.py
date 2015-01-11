754fb220c910e96a8e35caf8013bb3fcb6c79c5b
Christina Zou <czou@twitter.com>
2015-01-10 19:56:40 -0800

    more hcl stuff

--- hcl_compute.py
+++ hcl_compute.py
@@ -1,8 +1,9 @@
 import rpy2
 
 #dictionary of route variable means and variances
-route_var_params = {'turns': }
-
+route_params = {'directions': [10, 10], 'traffic': [10, 10], 'speed': [0.7, 0.3]}
+directions_scores = {''}
+parse_interval = 30 #30 seconds
 def chunk_hcl(route_data):
 
 	#r changepoint module: detect changepoints
@@ -13,38 +14,59 @@ def chunk_hcl(route_data):
 
 	#any segments that are too short ...?
 
+#parse a list of dicts
+def parse_dicts(input):
+	
+	directions = []
+	traffic_time = []
+	speed_limit = []
+	durations = []
 
+	for leg in input:
+		action = leg['action']
+		directions.append(convert_action_to_score(action))
+
+		traffic = float(leg['traffic_time'])/float(leg['base_time'])
+		traffic_time.append(traffic)
+
+		#get average speeds per link - for now
+		speed = sum(leg['speed_limit'])/len(leg['speed_limit'])
+		speed_limit.append(speed)
+
+		#durations - length per leg
+		durations.append(max(1, leg['traffic_time']/parse_interval))
+
+	return directions, traffic_time, speed_limit, durations
+
+def convert_action_to_score(action):
+
+	action = action.lower()
+
+	if(action.find("sharp") >= 0 or action.find("exit") >=0 or action.find("ramp") >= 0): 
+		return 5
+	if(action.find("merge") >= 0 or action.find("fork") >= 0 or action.find("loop") >= 0):
+		return 4
+	if(action.find("loop") >= 0 or action.find("turn") >= 0):
+		return 3
+	
+	return 0
+
+
+#route_data is a list of dicts
 def preprocess_hcl(route_data):
 
-	#durations = [3, 8, 4]
-	#speed = [10, 20, 30]
-	#traffic = [30, 30, 30]
+	directions, traffic, speed, durations = parse_dicts(input)
 
-
-	#directions - [0, 0, 1, 0, 0, ...] 1's are any sort of risky move: turn etc.
-
-
-depart, departAirport, arrive, arriveAirport, arriveLeft, arriveRight,
-leftLoop,leftUTurn,sharpLeftTurn, leftTurn, continue, slightRightTurn, rightTurn,sharpRightTurn,
-rightUTurn, rightLoop, leftExit, rightExit, leftRamp, rightRamp, leftFork, middleFork, rightFork,
-leftMerge, rightMerge, nameChange, trafficCircle, ferry ,leftRoundaboutExit1, leftRoundaboutExit2,
-leftRoundaboutExit3, leftRoundaboutExit4, leftRoundaboutExit5, leftRoundaboutExit6,
-leftRoundaboutExit7, leftRoundaboutExit8, leftRoundaboutExit9, leftRoundaboutExit10,
-leftRoundaboutExit11, leftRoundaboutExit12, rightRoundaboutExit1, rightRoundaboutExit2,
-rightRoundaboutExit3, rightRoundaboutExit4, rightRoundaboutExit5, rightRoundaboutExit6,
-rightRoundaboutExit7, rightRoundaboutExit8, rightRoundaboutExit9, rightRoundaboutExit10,
-rightRoundaboutExit11, rightRoundaboutExit12]
+	#directions - scoring, rule based (assume first is forward)
+	directions_processed = vectorize_speedOrDirections(durations, directions[1:])
 
 	#speed limit [30, 30, 10, 60, 60 ] : absolute change in speed limit
 	speed_abs_diff = [abs(speed[i] - speed[i-1]) for i in range(1, len(speed))]
-	speed_abs_diff_norm = normalize('speed', speed_abs_diff)
-	speed_processed = vectorize(durations, speed_abs_diff_norm)	
+	speed_processed = vectorize_speedOrDirections(durations, speed_abs_diff)	
 
 	#traffic (diff between traffic_time and base_time [3, 0, 0, 0, 4, 6, 8, 10, 10]
 	#get increase in driving time per minute of each interval
-	traffic_per = [traffic[i] / durations[i] for i in range(len(durations))]
-	traffic_per_norm = normalize('traffic', traffic_per)
-	traffic_per_norm_processed = vectorize(durations, traffic_per_norm)
+	traffic_processed = vectorize_traffic(durations, traffic)
 
 	#OTHER: weather - adds a multiplicative factor to everything 
 	#curvature - high curvatures
@@ -79,21 +101,30 @@ def exponential_smoothing(input):
 
 	return smoothed
 
-def get_direction(direction_input):
-
-
-
 #get in lists of timestamps at which var changes occur, values within each interval
 #return a time series vector
-def vectorize(durations, vals):
+def vectorize_speedOrDirections(durations, vals):
 	variable_vector = [0] * durations[0]
 	#0 at start
 	for i in range(len(vals)):
 		#change at a timestamp
+
 		variable_vector.extend([vals[i]])
 
 		#no change in the interval
-		variable_vector.extend([0] * durations[i+1])
+		if durations[i+1] >= 2:
+			variable_vector.extend([0] * (durations[i+1]-1))
+
+	return variable_vector
+
+#return a time series vector
+def vectorize_traffic(durations, vals):
+
+	variable_vector = []
+	#0 at start
+	for i in range(len(vals)):
+		#change at a timestamp
+		variable_vector.extend([vals[i]] * durations[i])
 
 	return variable_vector
 
@@ -103,11 +134,14 @@ def normalize(var_name, var_vec):
 	[var_mean, var_variance] = route_params[var_name]
 	
 	var_vec = [value - var_mean for value in var_vec]
-	var_vec = [value/value_variance for value in var_vec]
+	var_vec = [value/var_variance for value in var_vec]
 
 	return var_vec
 
 def compute_hcl(route_data):
+
+	#get inputs
+	parse_dicts(input)
 	#get processed values
 	speed, traffic, directions = preprocess(route_data)
 
@@ -116,7 +150,7 @@ def compute_hcl(route_data):
 
 	#calculate the weighted averages of the series
 	smoothed = exponential_smoothing(aggregate_ts)
-	
+
 	#for every 5 seconds, calculate PCL: weighted average of average values for each variable
-	
+
 
