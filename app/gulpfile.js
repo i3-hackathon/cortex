78a5a47afd5e7fc2e8ac9e35002c293b231ec443
Zethus Suen <zsuen@netflix.com>
2015-01-10 21:21:25 -0800

    adding materialize, updating gulpfile

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var del = require('del');
 
 gulp.task('sass', function() {
-  gulp.src('./src/sass/*.scss')
+  gulp.src('./src/sass/application.scss')
     .pipe(sass())
     .pipe(gulp.dest('./dist/css'))
+});
+
+gulp.task('clean', function() {
+  del([
+    'dist/**',
+    ])
 });
